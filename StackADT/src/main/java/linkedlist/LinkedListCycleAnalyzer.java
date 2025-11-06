aec6a99764cc5e9ec6d5bbb9116dc848cc3feb2a
SKIND0 <skind@student.touro.edu>
2025-11-06 15:00:59 -0500

    Update LinkedListCycleAnalyzer.java

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -6,6 +6,41 @@ import support.LLNode;
 
 public class LinkedListCycleAnalyzer<T> {
     public static <T> CycleInfo detectCycleInfo(LLNode<T> head){
-        throw new NotImplementedException("TODO: implement this method");
+        //throw new NotImplementedException("TODO: implement this method");
+        if (head == null) { //empty list.
+            return new CycleInfo(-1, 0);
+        }
+
+        LLNode<T> slow = head;
+        LLNode<T> fast = head;
+
+        while (fast != null && fast.getLink() != null){
+            slow = slow.getLink();
+            fast = fast.getLink().getLink();
+            if (slow == fast){
+                break;
+            }
+        }
+
+        if (fast == null || fast.getLink() == null){ //no cycle.
+            return new CycleInfo(-1, 0);
+        }
+
+        slow = head;
+        int entryIndex = 0;
+        while (slow != fast) {
+            slow = slow.getLink();
+            fast = fast.getLink();
+            entryIndex++;
+        }
+
+        int cycleLength = 1;
+        fast = slow.getLink();
+
+        while (fast != slow){
+            fast = fast.getLink();
+            cycleLength++;
+        }
+        return new CycleInfo(entryIndex, cycleLength);
     }
 }
